5105bf15ca92c801a55546b01856bdfb2215e881
guigawiller <guilhermewiller@hotmail.com>
2019-04-30 13:07:59 -0300

    att

--- TrabalhoLab/src/trabalholab/TrabalhoLab.java
+++ TrabalhoLab/src/trabalholab/TrabalhoLab.java
@@ -29,11 +29,8 @@ public class TrabalhoLab {
         LerArquivo teste = new LerArquivo();
 //        System.out.println(teste.calcularHash("love"));
 //        System.out.println(teste.calcularHash("slaughter"));
-//        System.out.println(teste.acharPalavra("slaughter"));
-//        teste.criarBaseDados();
+        System.out.println(teste.acharPalavra("four word date crush"));
 //        teste.criarTabelaHash();
-        int a = Integer.parseInt(teste.acharPalavra("marriage proposal").get(0));
-        teste.recuperaFilme(a);
     }
 
 }
